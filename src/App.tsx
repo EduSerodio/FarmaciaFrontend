c2f702d5ed563001275e777d97f000b99c734907
EduSerodio <dudu9090@outlook.com.br>
2023-11-09 12:28:40 -0300

    Implementação do CRUD de categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
+import FormsCategorias from './components/categoria/formCategorias/FormsCategorias';
+import ListaCategorias from './components/categoria/listaCategorias/ListaCategorias';
+import DeleteCategorias from './components/categoria/deleteCategorias/DeleteCategorias';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div className='min-h-[80vh]'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/categoria" element={<ListaCategorias />} />
+          <Route path="/cadastroCategoria" element={<FormsCategorias />} />
+          <Route path="/editarCategoria/:id" element={<FormsCategorias />} />
+          <Route path="/deletarCategoria/:id" element={<DeleteCategorias />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,17 @@
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
         <>
             <div className='w-full bg-lime-600 text-white
                 flex justify-center py-4'>
-            
+
                 <div className="container flex justify-between text-lg">
-                    Farmacia Front end
+                    <Link to='/home' className='text-2xl font-bold'>Farmacia</Link>
 
                     <div className='flex gap-4'>
-                        Categoria
-                        Cadastrar categoria
-                        Sair
+                        <Link to='/categoria' className='hover:underline'>Categorias</Link>
+                        <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar Categoria</Link>
                     </div>
                 </div>
             </div>
